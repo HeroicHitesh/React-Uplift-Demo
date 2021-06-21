77ef2afde2ade119e9c1b6d09da77996a2ab303a
HeroicHitesh <email.hiteshkumar@gmail.com>
2021-06-21 19:51:44 +0530

    Add LearningReact Component

---
+++ conditional-rendering/src/components/LearningReact.js
@@ -0,0 +1,15 @@
+import reactLogo from '../reactLogo.svg';
+import '../App.css';
+
+function LearningReact() {
+    return (
+        <>
+            <img src={reactLogo} className="App-logo" alt="logo" />
+            <p>
+            Learning React
+            </p>
+        </>
+    );
+}
+
+export default LearningReact;
