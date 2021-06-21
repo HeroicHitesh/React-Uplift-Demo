fab4eece48d974d8c883c111b50db2ffe645cdbe
HeroicHitesh <email.hiteshkumar@gmail.com>
2021-06-21 19:52:01 +0530

    Add LearningRedux Component

---
+++ conditional-rendering/src/components/LearningRedux.js
@@ -0,0 +1,16 @@
+import reduxLogo from '../reduxLogo.svg';
+import '../App.css';
+
+function LearningRedux() {
+    return (
+        <>
+            <img src={reduxLogo} className="App-logo" alt="logo" />
+            <p>
+            <br></br>
+            Learning Redux
+            </p>
+        </>
+    );
+}
+
+export default LearningRedux;
