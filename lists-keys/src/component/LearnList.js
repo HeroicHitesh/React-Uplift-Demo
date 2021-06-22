76bc667d1f129536ffa7a9a7531cb9eb314d166e
HeroicHitesh <email.hiteshkumar@gmail.com>
2021-06-22 19:51:22 +0530

    fix: Replace index with ids to avoid side effects

--- lists-keys/src/component/LearnList.js
+++ lists-keys/src/component/LearnList.js
@@ -1,18 +1,28 @@
 import React from "react";
 
 export default function LearnList() {
+  // const learnThis = [
+  //   "HTML/CSS/JS",
+  //   "React (currently learning)",
+  //   "Redux",
+  //   "GraphQL",
+  //   "Progressive Web Apps (PWAs)",
+  //   "React Native (for app dev)",
+  // ];
   const learnThis = [
-    "HTML/CSS/JS",
-    "React (currently learning)",
-    "Redux",
-    "GraphQL",
-    "Progressive Web Apps (PWAs)",
-    "React Native (for app dev)",
+    { id: 1, value: "HTML/CSS/JS" },
+    { id: 2, value: "React (currently learning)" },
+    { id: 3, value: "Redux" },
+    { id: 4, value: "GraphQL" },
+    { id: 5, value: "Progressive Web Apps (PWAs)" },
+    { id: 6, value: "React Native (for app dev)" },
   ];
+
   return (
     <ul>
-      {learnThis.map((tech, index) => (
-        <li key={index}>{tech}</li>
+      {/* Avoid using index as key to avoid any side effects */}
+      {learnThis.map((tech) => (
+        <li key={tech.id}>{tech.value}</li>
       ))}
     </ul>
   );
