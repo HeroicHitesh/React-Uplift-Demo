0c061409dd63392164a1d9743902a9eaef89bf34
HeroicHitesh <email.hiteshkumar@gmail.com>
2021-06-22 19:56:12 +0530

    feat: Show flow of data b/w components using props

--- lists-keys/src/App.js
+++ lists-keys/src/App.js
@@ -2,9 +2,18 @@ import LearnList from "./component/LearnList.js";
 import "./App.css";
 
 function App() {
+  // Hard-coded data in const array. In real-world scenarios mostly an API endpoint will be used to fetch data
+  const learnThis = [
+    { id: 1, value: "HTML/CSS/JS" },
+    { id: 2, value: "React (currently learning)" },
+    { id: 3, value: "Redux" },
+    { id: 4, value: "GraphQL" },
+    { id: 5, value: "Progressive Web Apps (PWAs)" },
+    { id: 6, value: "React Native (for app dev)" },
+  ];
   return (
     <header className="App-header">
-      <LearnList />
+      <LearnList learnThis={learnThis} />
     </header>
   );
 }
--- lists-keys/src/component/LearnList.js
+++ lists-keys/src/component/LearnList.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-export default function LearnList() {
+export default function LearnList(props) {
+  // Use props to pass data between components
+  const learnThis = props.learnThis;
+
   // const learnThis = [
   //   "HTML/CSS/JS",
   //   "React (currently learning)",
@@ -9,14 +12,14 @@ export default function LearnList() {
   //   "Progressive Web Apps (PWAs)",
   //   "React Native (for app dev)",
   // ];
-  const learnThis = [
-    { id: 1, value: "HTML/CSS/JS" },
-    { id: 2, value: "React (currently learning)" },
-    { id: 3, value: "Redux" },
-    { id: 4, value: "GraphQL" },
-    { id: 5, value: "Progressive Web Apps (PWAs)" },
-    { id: 6, value: "React Native (for app dev)" },
-  ];
+  // const learnThis = [
+  //   { id: 1, value: "HTML/CSS/JS" },
+  //   { id: 2, value: "React (currently learning)" },
+  //   { id: 3, value: "Redux" },
+  //   { id: 4, value: "GraphQL" },
+  //   { id: 5, value: "Progressive Web Apps (PWAs)" },
+  //   { id: 6, value: "React Native (for app dev)" },
+  // ];
 
   return (
     <ul>
